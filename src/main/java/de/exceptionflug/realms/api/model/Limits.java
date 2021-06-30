85f4696785a8bdeaec0dfe3088d708668676c6dc
Nico <n.britze@exceptionflug.de>
2021-06-30 19:10:45 +0200

    Add equals, hashCode and toString methods to Limits

--- src/main/java/de/exceptionflug/realms/api/model/Limits.java
+++ src/main/java/de/exceptionflug/realms/api/model/Limits.java
@@ -1,5 +1,7 @@
 package de.exceptionflug.realms.api.model;
 
+import java.util.Objects;
+
 /**
  * This model class specifies the limits of the realm according to it's current boost level.
  *
@@ -105,4 +107,33 @@ public class Limits {
                 predefinedPlugins, maxPlayers, subdomain);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Limits limits = (Limits) o;
+        return cpuLimit == limits.cpuLimit && memoryMegabytes == limits.memoryMegabytes && diskMegabytes == limits.diskMegabytes && loadedWorldsCount == limits.loadedWorldsCount && customGroupsCount == limits.customGroupsCount && unusedShutdownMinutes == limits.unusedShutdownMinutes && stoppedUnloadMinutes == limits.stoppedUnloadMinutes && customPlugins == limits.customPlugins && predefinedPlugins == limits.predefinedPlugins && maxPlayers == limits.maxPlayers && subdomain == limits.subdomain;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpuLimit, memoryMegabytes, diskMegabytes, loadedWorldsCount, customGroupsCount, unusedShutdownMinutes, stoppedUnloadMinutes, customPlugins, predefinedPlugins, maxPlayers, subdomain);
+    }
+
+    @Override
+    public String toString() {
+        return "Limits{" +
+                "cpuLimit=" + cpuLimit +
+                ", memoryMegabytes=" + memoryMegabytes +
+                ", diskMegabytes=" + diskMegabytes +
+                ", loadedWorldsCount=" + loadedWorldsCount +
+                ", customGroupsCount=" + customGroupsCount +
+                ", unusedShutdownMinutes=" + unusedShutdownMinutes +
+                ", stoppedUnloadMinutes=" + stoppedUnloadMinutes +
+                ", customPlugins=" + customPlugins +
+                ", predefinedPlugins=" + predefinedPlugins +
+                ", maxPlayers=" + maxPlayers +
+                ", subdomain=" + subdomain +
+                '}';
+    }
 }
