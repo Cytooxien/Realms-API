81cada3d58dcb0bb8940873a99a8e42c4c8899eb
Dannyx <dannyx@dannyx.de>
2025-07-26 19:07:31 +0200

    Update src/main/java/de/cytooxien/realms/api/enums/JoinReason.java
    Co-authored-by: Sascha Bartl <DerMistkaefer@users.noreply.github.com>

--- src/main/java/de/cytooxien/realms/api/enums/JoinReason.java
+++ src/main/java/de/cytooxien/realms/api/enums/JoinReason.java
@@ -2,9 +2,23 @@ package de.cytooxien.realms.api.enums;
 
 public enum JoinReason {
 
+    /**
+     * The player joined the realm directly, by connecting via server address.
+     */
     DIRECT,
-    LOBBY,
-    JOINME,
-    FRIEND_JUMP
 
+    /**
+     * The player joined the realm through the lobby.
+     */
+    LOBBY,
+
+    /**
+     * The player joined using a "JoinMe" invitation.
+     */
+    JOINME,
+
+    /**
+     * The player joined by jumping to a friend.
+     */
+    FRIEND_JUMP
 }
