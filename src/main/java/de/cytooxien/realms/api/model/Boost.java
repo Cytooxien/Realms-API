035e908a2b6b31f90c3da940cb0821f418f9cb39
Dannyx <dannyx@dannyx.de>
2025-07-27 23:30:45 +0200

    refactor: clean up Boost.java & turn it into a `record`

--- src/main/java/de/cytooxien/realms/api/model/Boost.java
+++ src/main/java/de/cytooxien/realms/api/model/Boost.java
@@ -1,21 +1,8 @@
 package de.cytooxien.realms.api.model;
 
-import java.util.Objects;
 import java.util.UUID;
 
-public class Boost {
-
-    private final UUID playerId;
-    private final long expireTimestamp;
-
-    private Boost(UUID playerId, long expireTimestamp) {
-        this.playerId = playerId;
-        this.expireTimestamp = expireTimestamp;
-    }
-
-    public static Boost create(UUID playerId, long expireTimestamp) {
-        return new Boost(playerId, expireTimestamp);
-    }
+public record Boost (UUID playerId, long expireTimestamp) {
 
     public UUID getPlayerId() {
         return playerId;
@@ -32,18 +19,5 @@ public class Boost {
                 "playerId=" + playerId +
                 ", expireTimestamp=" + expireTimestamp +
                 '}';
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(playerId, expireTimestamp);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Boost that = (Boost) o;
-        return playerId == that.playerId && expireTimestamp == that.expireTimestamp;
     }
 }
